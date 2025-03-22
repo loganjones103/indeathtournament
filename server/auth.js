efc28b00a9461b18f2ca9437727c3b218f405fc8
loganjones103 <logan.jones103@gmail.com>
2025-03-22 18:17:46 -0500

    login url update

--- server/auth.js
+++ server/auth.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5000/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "https://in-death-api.onrender.com/auth/google/callback",
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
