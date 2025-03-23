d1a9adefc8245ff78007244be5467d972c52b2ed
loganjones103 <logan.jones103@gmail.com>
2025-03-22 19:13:26 -0500

    More CORS again

--- server/server.js
+++ server/server.js
@@ -53,11 +53,13 @@ app.use(session({
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     cookie: {
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         httpOnly: true,
+        sameSite: "None",
         maxAge: 24 * 60 * 60 * 1000,
     }
 }));
+
 
 // ✅ Passport
 app.use(passport.initialize());
